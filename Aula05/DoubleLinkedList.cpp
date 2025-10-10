67ba91b58da5b0859fb2faadb1bead42a7038904
Jadson Teixeira <jadsontxm@gmail.com>
2025-10-10 13:13:54 -0300

    Atualizando

---
+++ Aula05/DoubleLinkedList.cpp
@@ -0,0 +1,104 @@
+#include "DoubleLinkedList.hpp"
+
+DoubleLinkedList::DoubleLinkedList() {
+    this->head = this->tail = nullptr;
+    this->legth = 0;
+}
+
+DoubleLinkedList::DoubleLinkedList(const DoubleLinkedList & list) {
+
+}
+
+DoubleLinkedList::~DoubleLinkedList() {
+    this->clear();
+}
+
+bool DoubleLinkedList::isEmpty() const {
+    return this->head == nullptr;
+}
+
+int DoubleLinkedList::getLength() const {
+    return this->legth;
+}
+
+void DoubleLinkedList::clear() {
+    if (!this->isEmpty()) {
+        Node * current = this->head;
+        Node * aux;
+
+        while (current != nullptr) {
+            aux = current;
+            current = current->next;
+            delete aux;
+        }
+
+        this->head = this->tail = nullptr;
+        this->legth = 0;
+    }
+}
+
+void DoubleLinkedList::print(Order order) const {
+    Node * current = (order == Order::FORWARD) ? this->head : this->tail;
+
+    while (current != nullptr) {
+        cout << current->key << " ";
+        current = (order == Order::FORWARD) ? current->next : current->previous;
+    }
+}
+
+const Node * DoubleLinkedList::search(int key) const {
+    Node * current = this->head;
+
+    while (current != nullptr && current->key < key) {
+        current = current->next;
+    }
+
+    if (current != nullptr && current->key == key) {
+        return current;
+    }
+
+    return nullptr;
+}
+
+bool DoubleLinkedList::insert(int key) {
+    Node * current = this->head;
+
+    while (current != nullptr && current->key < key) {
+        current = current->next;
+    }
+
+    if (current != nullptr && current->key == key) {
+        return false;
+    } else {
+        Node * newNode = new Node();
+        newNode->key = key;
+        if (current == this->head) {
+            newNode->next = this->head;
+            this->head = newNode;
+            newNode->previous = nullptr;
+            if (newNode->next != nullptr) {
+                newNode->next->previous = newNode;
+            } else {
+                this->tail = newNode;
+            }
+        }
+
+        return true;
+    }
+}
+
+bool DoubleLinkedList::remove(int key) {
+
+}
+
+void DoubleLinkedList::concatenate(const DoubleLinkedList & list) {
+
+}
+
+const DoubleLinkedList & DoubleLinkedList::operator=(const DoubleLinkedList & list) {
+
+}
+
+const DoubleLinkedList & DoubleLinkedList::operator+(const DoubleLinkedList & list) {
+
+}
---
+++ Aula05/DoubleLinkedList.hpp
@@ -0,0 +1,48 @@
+#ifndef _DOUBLE_LINKED_LIST_
+#define _DOUBLE_LINKED_LIST_
+
+#include <iostream>
+
+using std::cout; // puxa apenas a função do cout
+
+struct Node {
+    Node * previous;
+    int key;
+    Node * next;
+};
+
+enum Order {
+    FORWARD,
+    BACKWARD
+};
+
+class DoubleLinkedList {
+    private:
+        Node * head;
+        Node * tail;
+
+        int legth;
+
+    public:
+        DoubleLinkedList();
+        DoubleLinkedList(const DoubleLinkedList & list); // const deixa claro que a list não será modificada dentro do método
+
+        ~DoubleLinkedList();
+
+        bool isEmpty() const;
+        int getLength() const;
+
+        void clear();
+        void print(Order order = Order::FORWARD) const;
+
+        const Node * search(int key) const;
+        bool insert(int key);
+        bool remove(int key);
+
+        void concatenate(const DoubleLinkedList & list);
+
+        const DoubleLinkedList & operator=(const DoubleLinkedList & list);
+        const DoubleLinkedList & operator+(const DoubleLinkedList & list);
+};
+
+#endif
